a5fd9c7befc1d173c086e9e17200d1d1ecd0154d
Błażej Kramarski <b.kramarski@gmail.com>
2015-06-05 00:26:06 +0200

    Using GetEncoders and GetModes in help message.

--- octogo.go
+++ octogo.go
@@ -5,55 +5,68 @@ import (
 	"fmt"
 	"github.com/Bajron/octogo/octogo"
 	"log"
+	"os"
 	"path/filepath"
+	"strings"
 )
 
 var inFile, outFile, mode string
 
-const (
-	DEFAULT_INPUT  = "in.(png|jpg|bmp|gif)"
-	DEFAULT_OUTPUT = "out.(png|jpg|bmp|gif)"
-	DEFAULT_MODE   = "(copy|gray|...)"
+var (
+	kDefaultInput  = "in.(png|jpg|gif)"
+	kDefaultOutput = "out.(png|jpg|bmp|gif)"
+	kDefaultMode   = "(copy|gray|...)"
 )
 
 func init() {
 	const (
-		INPUT_USAGE = "a file to read (jpg,png,gif or bmp)"
+		INPUT_USAGE = "a file to read"
 		OUPUT_USAGE = "output filename"
 		MODE_USAGE  = "processing function"
 	)
 
-	flag.StringVar(&inFile, "input", DEFAULT_INPUT, INPUT_USAGE)
-	flag.StringVar(&inFile, "f", DEFAULT_INPUT, INPUT_USAGE+" (shorthand for --input)")
+	kDefaultOutput = "out.(" + strings.Join(octogo.GetEncoders(), "|") + ")"
+	kDefaultMode = "(" + strings.Join(octogo.GetModes(), "|") + ")"
 
-	flag.StringVar(&outFile, "output", DEFAULT_OUTPUT, OUPUT_USAGE)
-	flag.StringVar(&outFile, "o", DEFAULT_OUTPUT, OUPUT_USAGE+" (shorthand for --output)")
+	flag.StringVar(&inFile, "input", kDefaultInput, INPUT_USAGE)
+	flag.StringVar(&inFile, "f", kDefaultInput, INPUT_USAGE+" (shorthand for --input)")
 
-	flag.StringVar(&mode, "mode", DEFAULT_MODE, MODE_USAGE)
-	flag.StringVar(&mode, "m", DEFAULT_MODE, MODE_USAGE+" (shorthand for --mode)")
+	flag.StringVar(&outFile, "output", kDefaultOutput, OUPUT_USAGE)
+	flag.StringVar(&outFile, "o", kDefaultOutput, OUPUT_USAGE+" (shorthand for --output)")
+
+	flag.StringVar(&mode, "mode", kDefaultMode, MODE_USAGE)
+	flag.StringVar(&mode, "m", kDefaultMode, MODE_USAGE+" (shorthand for --mode)")
 }
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr,
+			"Usage:\n"+
+				"\t%s [options] [input file] [output file]\n\n",
+			os.Args[0])
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
-	if flag.NArg() > 0 && inFile == DEFAULT_INPUT {
+	if flag.NArg() > 0 && inFile == kDefaultInput {
 		inFile = flag.Arg(0)
 	}
-	if flag.NArg() > 1 && outFile == DEFAULT_OUTPUT {
+	if flag.NArg() > 1 && outFile == kDefaultOutput {
 		outFile = flag.Arg(1)
 	}
 
-	if inFile == DEFAULT_INPUT {
+	if inFile == kDefaultInput {
 		fmt.Printf("You need to provide input file!\n")
 		flag.Usage()
 		return
 	}
 
-	if outFile == DEFAULT_OUTPUT {
+	if outFile == kDefaultOutput {
 		outFile = "out.png"
 	}
 
-	if mode == DEFAULT_MODE {
+	if mode == kDefaultMode {
 		mode = "copy"
 	}
 
