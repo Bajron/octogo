c21311024d484cb42756a04169b5b3eb5cd1e257
Błażej Kramarski <b.kramarski@gmail.com>
2015-06-04 16:57:49 +0200

    Basic command line input handling.

---
+++ octogo.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var inFile, outFile string
+
+const (
+	DEFAULT_INPUT  = "in.(png|jpg|bmp|gif)"
+	DEFAULT_OUTPUT = "out.(png|jpg|bmp|gif)"
+)
+
+func init() {
+	const (
+		INPUT_USAGE = "specifies a file to read (jpg,png,gif or bmp)"
+		OUPUT_USAGE = "specifies output filename"
+	)
+
+	flag.StringVar(&inFile, "input", DEFAULT_INPUT, INPUT_USAGE)
+	flag.StringVar(&inFile, "f", DEFAULT_INPUT, INPUT_USAGE+" (shorthand for --input)")
+
+	flag.StringVar(&outFile, "output", DEFAULT_OUTPUT, OUPUT_USAGE)
+	flag.StringVar(&outFile, "o", DEFAULT_OUTPUT, OUPUT_USAGE+" (shorthand for --output)")
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() > 0 && inFile == DEFAULT_INPUT {
+		inFile = flag.Arg(0)
+	}
+	if flag.NArg() > 1 && outFile == DEFAULT_OUTPUT {
+		outFile = flag.Arg(1)
+	}
+
+	if inFile == DEFAULT_INPUT {
+		fmt.Printf("You need to provide input file!\n")
+		flag.Usage()
+		return
+	}
+
+	if outFile == DEFAULT_OUTPUT {
+		outFile = "out.png"
+	}
+
+	fmt.Printf("Processing %s -> %s!\n", inFile, outFile)
+}
